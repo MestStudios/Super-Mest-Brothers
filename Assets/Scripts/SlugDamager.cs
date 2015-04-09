1bdc03acee6c8e64c9081db8f8af08f73576f6b7
Josué González Barrera <josu.vicio@gmail.com>
2015-04-09 20:52:52 +0200

    Añadidas animaciones jugador. Añadido script enemigo para matar al jugador.

---
+++ Assets/Scripts/SlugDamager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlugDamager : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            // Matar PJ
+        }
+    }
+}
